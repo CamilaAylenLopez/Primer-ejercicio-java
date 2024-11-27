a39f08d53cb2754dedf2cd3c677b022c2fffee64
Camila Aylen Lopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2024-11-27 18:52:58 +0100

    protecto completo

---
+++ src/main/java/com/mycompany/ecuacion_segundo_grado/Ecuacion_segundo_grado.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecuacion_segundo_grado;
+
+/**
+ *
+ * @author camil
+ */
+import java.util.Scanner;
+
+public class Ecuacion_segundo_grado {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double a, b, c, x1, x2, discriminante;
+
+        System.out.print("Ingrese a: ");
+        a = teclado.nextDouble();
+        System.out.print("Ingrese b: ");
+        b = teclado.nextDouble();
+        System.out.print("Ingrese c: ");
+        c = teclado.nextDouble();
+
+        discriminante = Math.pow(b, 2) - (4 * a * c);
+
+        if ((a == 0) && (b == 0) && (c == 0)) {
+            System.out.println("Tiene infinitas soluciones.");
+        }
+        else if (discriminante > 0) {
+            x1 = ((-b + Math.sqrt(discriminante)) / (2 * a));
+            x2 = ((-b - Math.sqrt(discriminante)) / (2 * a));
+            System.out.print("La solución es x1 es " + x1 + " la solución es x2 es " + x2);
+        }
+        else if (discriminante < 0) {
+            System.out.print("Tiene soluciones no reales.");
+        }
+        else if (discriminante == 0) {
+            try {
+                x1 = ((-b + Math.sqrt(discriminante)) / (2 *a));
+            } catch (Exception e) {
+                x1 = ((-b - Math.sqrt(discriminante))/ (2 *a));
+            }
+            System.out.print("La solución es " + x1);
+        }
+    }
+}
