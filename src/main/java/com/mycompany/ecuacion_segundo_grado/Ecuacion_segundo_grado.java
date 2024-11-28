6b9a459f4e2ebc5c959876bc7da73e32257e291c
Camila Aylen Lopez <115021067+CamilaAylenLopez@users.noreply.github.com>
2024-11-28 19:45:14 +0100

    update

--- src/main/java/com/mycompany/ecuacion_segundo_grado/Ecuacion_segundo_grado.java
+++ src/main/java/com/mycompany/ecuacion_segundo_grado/Ecuacion_segundo_grado.java
@@ -14,14 +14,23 @@ public class Ecuacion_segundo_grado {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        double a, b, c, x1, x2, discriminante;
+        double a = 0, b = 0, c = 0, x1 = 0, x2 = 0, discriminante = 0;
+        boolean numerosValidos = false;
 
-        System.out.print("Ingrese a: ");
-        a = teclado.nextDouble();
-        System.out.print("Ingrese b: ");
-        b = teclado.nextDouble();
-        System.out.print("Ingrese c: ");
-        c = teclado.nextDouble();
+        while (!numerosValidos) {
+            try {
+                System.out.print("Ingrese a: ");
+                a = teclado.nextDouble();
+                System.out.print("Ingrese b: ");
+                b = teclado.nextDouble();
+                System.out.print("Ingrese c: ");
+                c = teclado.nextDouble();
+                numerosValidos = true;
+            } catch (Exception e) {
+                System.out.println("Error debe ingresar un número válido!!");
+                teclado.next();
+            }
+        }        
 
         discriminante = Math.pow(b, 2) - (4 * a * c);
 
@@ -43,6 +52,9 @@ public class Ecuacion_segundo_grado {
                 x1 = ((-b - Math.sqrt(discriminante))/ (2 *a));
             }
             System.out.print("La solución es " + x1);
+        }
+        else{
+            System.out.print("Ocurrio un error...");
         }
     }
 }
